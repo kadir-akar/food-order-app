bfa95d40705a573dd41b6649e826f05c610b6d25
Kadir Akar <aakarkadir@gmail.com>
2023-02-28 18:04:28 +0300

    some ui folders and features added

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import { HeaderCardButton } from "./HeaderCartButton";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCardButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="a table full of food!" />
+      </div>
+    </Fragment>
+  );
+};
+export default Header;
---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,9 @@
+import classes from "./Modal.module.css";
+const Modal = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+export default Modal;
