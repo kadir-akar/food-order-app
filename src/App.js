c24b58c50baf06070c76ee2f5d5cf86969b97394
Kadir Akar <aakarkadir@gmail.com>
2023-03-24 23:05:58 +0300

    cart info added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import { Fragment } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import { useState } from "react";
+import CartProvider from "./store/CartProvider";
+
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
   const showCartHandler = () => {
@@ -12,13 +13,13 @@ function App() {
     setCartIsShown(false);
   };
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,9 +1,27 @@
 import classes from "./MealItemForm.module.css";
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
-export const MealItemForm = () => {
+export const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    if (
+      enteredAmount.trim().length === 0 ||
+      +enteredAmount < 1 ||
+      +enteredAmount > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(+enteredAmount);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount",
@@ -15,6 +33,7 @@ export const MealItemForm = () => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
